7c42abf8cfb29017323b77e429e435a4a0025e81
NguyenGiaHy123 <89922176+NguyenGiaHy123@users.noreply.github.com>
2023-05-31 12:00:19 +0700

    depodfa

--- src/Component/SlidBar/Slidebar.tsx
+++ src/Component/SlidBar/Slidebar.tsx
@@ -3,12 +3,12 @@ import React,{FunctionComponent} from 'react'
 import { AiFillYoutube,AiOutlineFacebook } from "react-icons/ai";
 import { BsTiktok } from "react-icons/bs";
 import { Icorlor } from '../../Type';
-
+import imagehy from '../../image/hy.jpg'
 export const  Slidebar:FunctionComponent<Icorlor>=({theme,setTheme})=> {
   return (
     <div className={`flex flex-col  h-full bg-white dark:bg-gray-900 justify-center dark:shadow-custom-light items-center p-5  py-5`}>
-       <img src='https://files.combyne.com/97834614f86f0ebc0e4d6e229280105b_image.jpg' alt='user avarta'
-        className='h-36 w-36 rounded-full'
+       <img src={imagehy} alt='user avarta'
+        className='h-44 w-44 rounded-full '
        />
        <h3 className='mt-3 mb-4 text-3xl font-bold' style={{fontFamily:" Dancing Script"}}>
           <span className='text-green-600'>Hy</span> Nguyễn 
@@ -17,8 +17,8 @@ export const  Slidebar:FunctionComponent<Icorlor>=({theme,setTheme})=> {
        <p className={`mb-4 w-full text-center  bg-gray-200  dark:bg-dark-700  rounded-xl p-1 cursor-pointer`}  style={{fontFamily:"Labrada cursive "}}>App developer</p>
        <div className='flex gap-10 mb-5'>
         <AiFillYoutube size={23} className='text-green-600'/>
-        <AiOutlineFacebook size={23 } className='text-green-600' />
-        <BsTiktok size={23} className='text-green-600'/>
+        <a href='https://www.facebook.com/giahy.nguyen.1671?mibextid=LQQJ4d'> <AiOutlineFacebook size={23 } className='text-green-600' /></a> 
+       <BsTiktok size={23} className='text-green-600'/>
        </div>
 
        {/* address */}
